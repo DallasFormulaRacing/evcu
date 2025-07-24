6088f79d84a408ee402cd6e5b66efe6fae9989b3
schommie <schommersebastian@gmail.com>
2025-07-24 18:46:16 -0500

    Basic CAN firmware
    Basic STM32cube project which properly configures clock timings and interrupt based CAN messaging for the stm32 nucleo h533RE

---
+++ CAN_BASICS/Core/Src/main.c
@@ -0,0 +1,347 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+COM_InitTypeDef BspCOMInit;
+
+FDCAN_HandleTypeDef hfdcan2;
+
+/* USER CODE BEGIN PV */
+FDCAN_TxHeaderTypeDef TxHeader;
+FDCAN_RxHeaderTypeDef RxHeader;
+uint8_t messageData[64];
+uint32_t messageID;
+int receivedFrame = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_FDCAN2_Init(void);
+/* USER CODE BEGIN PFP */
+void startupFDCAN(FDCAN_HandleTypeDef *hfdcan, FDCAN_TxHeaderTypeDef *tx_header,
+		void (*err_handler)(void));
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+
+	/* USER CODE BEGIN 1 */
+
+	/* USER CODE END 1 */
+
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_FDCAN2_Init();
+	/* USER CODE BEGIN 2 */
+	startupFDCAN(&hfdcan2, &TxHeader, &Error_Handler);
+	/* USER CODE END 2 */
+
+	/* Initialize leds */
+	//BSP_LED_Init(LED_GREEN);
+	/* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
+	//BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
+	/* Initialize COM1 port (115200, 8 bits (7-bit data + 1 stop bit), no parity */
+//	BspCOMInit.BaudRate = 115200;
+//	BspCOMInit.WordLength = COM_WORDLENGTH_8B;
+//	BspCOMInit.StopBits = COM_STOPBITS_1;
+//	BspCOMInit.Parity = COM_PARITY_NONE;
+//	BspCOMInit.HwFlowCtl = COM_HWCONTROL_NONE;
+//	if (BSP_COM_Init(COM1, &BspCOMInit) != BSP_ERROR_NONE) {
+//		Error_Handler();
+//	}
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
+
+		TxHeader.DataLength = 0;
+		HAL_StatusTypeDef status = HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan2,
+				&TxHeader, messageData);
+		if (status != HAL_OK) {
+			Error_Handler();
+		}
+		HAL_Delay(100);
+
+	}
+	/* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+	while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+	}
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV2;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLL1_SOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 4;
+	RCC_OscInitStruct.PLL.PLLN = 60;
+	RCC_OscInitStruct.PLL.PLLP = 2;
+	RCC_OscInitStruct.PLL.PLLQ = 12;
+	RCC_OscInitStruct.PLL.PLLR = 2;
+	RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1_VCIRANGE_3;
+	RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1_VCORANGE_WIDE;
+	RCC_OscInitStruct.PLL.PLLFRACN = 0;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK3;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Configure the programming delay
+	 */
+	__HAL_FLASH_SET_PROGRAM_DELAY(FLASH_PROGRAMMING_DELAY_2);
+}
+
+/**
+ * @brief FDCAN2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_FDCAN2_Init(void) {
+
+	/* USER CODE BEGIN FDCAN2_Init 0 */
+
+	/* USER CODE END FDCAN2_Init 0 */
+
+	/* USER CODE BEGIN FDCAN2_Init 1 */
+
+	/* USER CODE END FDCAN2_Init 1 */
+	hfdcan2.Instance = FDCAN2;
+	hfdcan2.Init.ClockDivider = FDCAN_CLOCK_DIV1;
+	hfdcan2.Init.FrameFormat = FDCAN_FRAME_CLASSIC;
+	hfdcan2.Init.Mode = FDCAN_MODE_INTERNAL_LOOPBACK;
+	hfdcan2.Init.AutoRetransmission = DISABLE;
+	hfdcan2.Init.TransmitPause = DISABLE;
+	hfdcan2.Init.ProtocolException = DISABLE;
+	hfdcan2.Init.NominalPrescaler = 4;
+	hfdcan2.Init.NominalSyncJumpWidth = 2;
+	hfdcan2.Init.NominalTimeSeg1 = 7;
+	hfdcan2.Init.NominalTimeSeg2 = 2;
+	hfdcan2.Init.DataPrescaler = 1;
+	hfdcan2.Init.DataSyncJumpWidth = 2;
+	hfdcan2.Init.DataTimeSeg1 = 7;
+	hfdcan2.Init.DataTimeSeg2 = 2;
+	hfdcan2.Init.StdFiltersNbr = 0;
+	hfdcan2.Init.ExtFiltersNbr = 0;
+	hfdcan2.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
+	if (HAL_FDCAN_Init(&hfdcan2) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN FDCAN2_Init 2 */
+
+	/* USER CODE END FDCAN2_Init 2 */
+
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
+
+	/* USER CODE END MX_GPIO_Init_1 */
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	/*Configure GPIO pin : PC4 */
+	GPIO_InitStruct.Pin = GPIO_PIN_4;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
+
+	/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+void configureFDCANTransmissionHeader(FDCAN_TxHeaderTypeDef *tx_header) {
+	tx_header->IdType = FDCAN_STANDARD_ID;
+	tx_header->TxFrameType = FDCAN_DATA_FRAME;
+	tx_header->DataLength = FDCAN_DLC_BYTES_0;
+	tx_header->ErrorStateIndicator = FDCAN_ESI_ACTIVE;
+	tx_header->BitRateSwitch = FDCAN_BRS_OFF;
+	tx_header->FDFormat = FDCAN_CLASSIC_CAN;
+	tx_header->TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+	tx_header->MessageMarker = 0;
+}
+
+void startupFDCAN(FDCAN_HandleTypeDef *hfdcan, FDCAN_TxHeaderTypeDef *tx_header,
+		void (*err_handler)(void)) {
+	HAL_StatusTypeDef hal_status;
+
+	if (HAL_FDCAN_GetState(hfdcan) != HAL_FDCAN_STATE_READY) {
+		err_handler();  // startupFDCAN called before hfdcan initialized.
+	}
+
+	// Enable CAN transceiver & interrupts
+	//stby_pin->digitalWrite(false);  // Enable the transceiver
+	hal_status = HAL_FDCAN_Start(hfdcan);
+	if (hal_status != HAL_OK) {
+		err_handler();
+	}
+
+	// Activate RX interrupts
+	hal_status = HAL_FDCAN_ActivateNotification(hfdcan,
+	FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
+	if (hal_status != HAL_OK) {
+		err_handler();
+	}
+
+	configureFDCANTransmissionHeader(tx_header);
+
+	if (HAL_FDCAN_GetState(hfdcan) == HAL_FDCAN_STATE_ERROR) {
+		err_handler(); // Make sure we're not in an error state
+	}
+}
+
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs) {
+	if ((RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE) != RESET) {
+		/* Retrieve RX messages from RX FIFO0 */
+		if (HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0, &RxHeader,
+				messageData) != HAL_OK) {
+			/* Reception Error */
+			Error_Handler();
+		}
+		receivedFrame = 1;
+		if (HAL_FDCAN_ActivateNotification(hfdcan,
+		FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0) != HAL_OK) {
+			/* Notification Error */
+			Error_Handler();
+		}
+	}
+}
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
