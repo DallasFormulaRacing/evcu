1882fd40e493c679574f5ba4dad8630a039d75c3
will kim <73252368+wxkim@users.noreply.github.com>
2025-08-19 21:14:46 -0500

    Update torque.py

--- torq-controller/torque.py
+++ torq-controller/torque.py
@@ -9,14 +9,31 @@ Inverter sends CAN status message back to VCU
 Motor spins
 Motor resolver (which is basically a really fast analog rotational speed sensor) sends back the angular velocity of the motor to the Inverter
 Inverter sends resolver data to VCU via CAN message
-Need Resolver-to-Digital Converter to decode analog resolver signals into digital signals
+Need Resolver-to-Digital Converter to decode analog resolver signals into digital signals (this is already done in the Inverter)
 '''
 
 '''
 From the  persepective of the torque controller algorithm running on the VCU, the torque controller is a closed loop control system.
 '''
+
+'''
+Goal: Develop a torque control algorithm that limits motor torque based on current vehicle dynamics, such as:
+- Wheel speed
+- Vehicle dynamics (IMU)
+- Throttle change
+- Torque smoothing techniques (i.e. make the slope less steep)
+'''
 import numpy as np
 import matplotlib.pyplot as plot
 import dataclasses as dc
  
-
+@dc.dataclass
+class APPS:
+    position: float = 0.0
+    max_position: float = 1.0  # Max position of the accelerator pedal
+    min_position: float = 0.0  # Min position of the accelerator pedal
+    
+@dc.dataclass
+class TorqueController:
+    desired_torque: float = 0.0
+    max_torque: float = 100.0
